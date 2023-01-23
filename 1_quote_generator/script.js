78d336983218b095195f506448b7604d0a978b2d
paudel54 <2sansrit@gmail.com>
2023-01-23 11:23:04 +0545

    added css loader, bug need to resolve

--- 1_quote_generator/script.js
+++ 1_quote_generator/script.js
@@ -3,6 +3,7 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // Get Quotes From API
 // async func 
@@ -10,8 +11,20 @@ const newQuoteBtn = document.getElementById('new-quote');
 //empty global var would be populated fetching api, converting to json obj and putting on global var
 let apiQuotes = [];
 
+//Show loading
+function loading() {
+    //when loader is going we want only loader be seen nothing else
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+//HIde loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 //Show new Quote 
 function newQuote() {
+    loading();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // console.log(quote);
@@ -28,13 +41,16 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    //set Quote , Hide Loader
     //adding text from quote api object
     quoteText.textContent = quote.text;
+    complete();
 }
 
 
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     // try catch allows to attempt to complete fetch req but if it doesn't work we can catch
     //error and do something with it
