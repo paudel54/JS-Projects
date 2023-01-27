fb1563eaf936a1fd0de021c213456710e2f8b303
paudel54 <2sansrit@gmail.com>
2023-01-27 09:05:08 +0545

    added calculation functionaloity

--- 3_calculator/script.js
+++ 3_calculator/script.js
@@ -4,25 +4,37 @@ const inputBtns = document.querySelectorAll('button');
 // shows 17 btn with class name
 const clearBtn = document.getElementById('clear-btn');
 
+//Making Operator FUnctionality
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 // Add Event Listeners for numbers, operators,decimal buttons
 function sendNumberValue(number) {
-    console.log(number);
-    // showing Numbers on calculator display , html id, with css textContent feature
-    // calculatorDisplay.textContent = number;
+    // once first value entered and hit plus sign we want to reset the display
+    if (awaitingNextValue === true) {
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    } else {
+        // console.log(number);
+        // showing Numbers on calculator display , html id, with css textContent feature
+        // calculatorDisplay.textContent = number;
 
-    //If current display value is 0 replace it with other number, if not 0 concatinate it. 
+        //If current display value is 0 replace it with other number, if not 0 concatinate it. 
 
-    const displayValue = calculatorDisplay.textContent;
-    // console.log(displayValue);
-    //  carries default value 0, given by calculatorDisplay.textContent
-    // initially secreen value is 0 , so when it's 0 number would be assingdd to input number, after 1st iteration
-    //the default value gets changed, and therefor number would be concatinated with previsous value
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
-
+        const displayValue = calculatorDisplay.textContent;
+        // console.log(displayValue);
+        //  carries default value 0, given by calculatorDisplay.textContent
+        // initially secreen value is 0 , so when it's 0 number would be assingdd to input number, after 1st iteration
+        //the default value gets changed, and therefor number would be concatinated with previsous value
+        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
 
 }
 
 function addDecimal() {
+    // if operator pressed, don't add decimal, on boolean value true is not need to defined , its defalut true
+    if (awaitingNextValue) return;
     // If no decimal, add one 
     if (!calculatorDisplay.textContent.includes('.')) {
         // added decimal after the dispaly numbers
@@ -30,22 +42,43 @@ function addDecimal() {
     }
 }
 
+function useOperator(operator) {
+    // converted text string to number and storing it , when operator are clicked comes here as operator arg
+    const currentValue = Number(calculatorDisplay.textContent);
+    // Assign first value if no value
+    // when opeator is clicked after taking Number Input then
+    if (!firstValue) {
+        firstValue = currentValue;
+    } else {
+        console.log('currentValue', currentValue);
+    }
+    // Ready for next value, store operator
+    awaitingNextValue = true;
+
+    operatorValue = operator;
+    console.log('firstValue', firstValue);
+    console.log('operator', operatorValue);
+}
+
+// event listeners
 inputBtns.forEach((inputBtn) => {
     // target input button with no classList, here numbers only have no classList
     if (inputBtn.classList.length === 0) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('operator')) {
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
         inputBtn.addEventListener('click', () => addDecimal());
     }
 });
 
-// Reset Display
+// Reset  all values, Display
 function resetAll() {
     calculatorDisplay.textContent = '0';
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
 }
 
 // Event listener for clear btn
-
 clearBtn.addEventListener('click', resetAll);
