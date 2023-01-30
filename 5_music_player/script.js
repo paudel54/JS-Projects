2f3385975dcd449bc24bfa73e3fd190c97d29418
paudel54 <2sansrit@gmail.com>
2023-01-30 21:40:52 +0545

    Project Music Player Completed will do some custom redesign

--- 5_music_player/script.js
+++ 5_music_player/script.js
@@ -147,8 +147,27 @@ function updateProgressBar(e) {
 
     }
 }
+// Set Progress Bar
+function setProgressBar(e) {
+    console.log(e);
+    const width = this.clientWidth;
+    console.log('Width', width);
+    // there is srcElement , and inside there is a clientWidth: 360 
+    const clickX = e.offsetX;
+    console.log('ClickX', clickX);
+    console.log(music);
+    // duration is property of audio/video. returns the value in second. 
+    const { duration } = music;
+    console.log(music);
+    console.log(clickX / width);
+    console.log((clickX / width) * duration);
+    // jump aroundonto music progress
+    music.currentTime = (clickX / width) * duration;
+}
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
